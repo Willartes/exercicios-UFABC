6660c6cecc640f340379d3564cb9d13ad1bc0b5c
william <william@pc-samsung.com.br>
2026-02-25 17:11:14 -0300

    exercicio atividade  D

--- VendingMachineF.py
+++ VendingMachineF.py
@@ -1,43 +1,41 @@
-while True:
-    try:
-        linha_configuracao = input().split()
-        if not linha_configuracao:
-            break
-            
-        valor_troco_alvo = int(linha_configuracao[0])
-        if valor_troco_alvo == 0:
-            break
-            
-        total_tipos_moedas = int(linha_configuracao[1])
-        
-        lista_de_moedas = []
-        while len(lista_de_moedas) < total_tipos_moedas:
-            lista_de_moedas.extend(map(int, input().split()))
-        
-        combinacoes_bits = 1
-        bit_objetivo = 1 << valor_troco_alvo
-        mascara_limite = (1 << (valor_troco_alvo + 1)) - 1
-        
-        sucesso = False
+import sys
 
-        for quantidade_moedas in range(1, valor_troco_alvo + 1):
-            novas_combinacoes = 0
-            for valor_moeda in lista_de_moedas:
-                novas_combinacoes |= (combinacoes_bits << valor_moeda)
-            
-            combinacoes_bits = novas_combinacoes & mascara_limite
-            
-            if combinacoes_bits & bit_objetivo:
-                print(quantidade_moedas)
-                print()
-                sucesso = True
+entrada_bruta = sys.stdin.read().split()
+if entrada_bruta:
+    fluxo_dados = iter(entrada_bruta)
+    
+    while True:
+        try:
+            valor_objetivo = int(next(fluxo_dados))
+            if valor_objetivo == 0:
                 break
             
-            if combinacoes_bits == 0:
-                break
-        
-        if not sucesso:
-            print("impossivel")
+            total_moedas = int(next(fluxo_dados))
+            lista_moedas = [int(next(fluxo_dados)) for _ in range(total_moedas)]
             
-    except EOFError:
-        break
+            bits_alcancaveis = 1
+            bit_alvo = 1 << valor_objetivo
+            mascara_bits = (1 << (valor_objetivo + 1)) - 1
+            
+            foi_possivel = False
+            
+            for quantidade in range(1, valor_objetivo + 1):
+                proximos_bits = 0
+                for valor_moeda in lista_moedas:
+                    proximos_bits |= (bits_alcancaveis << valor_moeda)
+                
+                bits_alcancaveis = proximos_bits & mascara_bits
+                
+                if bits_alcancaveis & bit_alvo:
+                    sys.stdout.write(str(quantidade)+"\n")
+                    foi_possivel = True
+                    break
+                
+                if bits_alcancaveis == 0:
+                    break
+            
+            if not foi_possivel:
+                sys.stdout.write("impossivel\n")
+                
+        except StopIteration:
+            break
